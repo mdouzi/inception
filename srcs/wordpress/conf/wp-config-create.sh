996f077da484d3d3f458d2dd3ab71d34653bf17e
mdouzi <mdouzi1069@gmail.com>
2024-08-22 16:55:22 -0400

    update

--- srcs/wordpress/conf/wp-config-create.sh
+++ srcs/wordpress/conf/wp-config-create.sh
@@ -1,56 +1,71 @@
 #!/bin/bash
-#---------------------------------------------------wp installation---------------------------------------------------#
-# wp-cli installation
+
+#---------------------------------------------------#
+#                  WP-CLI Installation              #
+#---------------------------------------------------#
+
+# Download wp-cli
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-# wp-cli permission
 chmod +x wp-cli.phar
-# wp-cli move to bin
 mv wp-cli.phar /usr/local/bin/wp
 
-# go to wordpress directory
+#---------------------------------------------------#
+#          WordPress Directory Permissions          #
+#---------------------------------------------------#
+
+# Navigate to the WordPress directory
 cd /var/www/wordpress
-# give permission to wordpress directory
-chmod -R 755 /var/www/wordpress/
-# change owner of wordpress directory to www-data
+
+# Set correct ownership and permissions for WordPress directory
 chown -R www-data:www-data /var/www/wordpress
-#---------------------------------------------------ping mariadb---------------------------------------------------#
-# check if mariadb container is up and running
-ping_mariadb_container() {
-    nc -zv mariadb 3306 > /dev/null # ping the mariadb container
-    return $? # return the exit status of the ping command
-}
-start_time=$(date +%s) # get the current time in seconds
-end_time=$((start_time + 20)) # set the end time to 20 seconds after the start time
-while [ $(date +%s) -lt $end_time ]; do # loop until the current time is greater than the end time
-    ping_mariadb_container # Ping the MariaDB container
-    if [ $? -eq 0 ]; then # Check if the ping was successful
-        echo "[========MARIADB IS UP AND RUNNING========]"
-        break # Exit the loop if MariaDB is up
-    else
+chmod -R 755 /var/www/wordpress
+
+#---------------------------------------------------#
+#            Waiting for MariaDB to Start           #
+#---------------------------------------------------#
+
+# Function to check MariaDB status
+wait_for_mariadb() {
+    for i in {1..20}; do
+        if nc -z mariadb 3306 > /dev/null 2>&1; then
+            echo "[========MARIADB IS UP AND RUNNING========]"
+            return 0
+        fi
         echo "[========WAITING FOR MARIADB TO START...========]"
-        sleep 1 # Wait for 1 second before trying again
-    fi
-done
-
-if [ $(date +%s) -ge $end_time ]; then # check if the current time is greater than or equal to the end time
+        sleep 1
+    done
     echo "[========MARIADB IS NOT RESPONDING========]"
-fi
-#---------------------------------------------------wp installation---------------------------------------------------##---------------------------------------------------wp installation---------------------------------------------------#
+    exit 1
+}
 
-# download wordpress core files
+# Call the function
+wait_for_mariadb
+
+#---------------------------------------------------#
+#               WordPress Installation              #
+#---------------------------------------------------#
+
+# Download the core WordPress files
 wp core download --allow-root
-# create wp-config.php file with database details
+
+# Create the wp-config.php file with database credentials
 wp core config --dbhost=mariadb:3306 --dbname="$MYSQL_DB" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --allow-root
-# install wordpress with the given title, admin username, password and email
+
+# Install WordPress with the given details
 wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_N" --admin_password="$WP_ADMIN_P" --admin_email="$WP_ADMIN_E" --allow-root
-#create a new user with the given username, email, password and role
+
+# Create a new user with specified credentials and role
 wp user create "$WP_U_NAME" "$WP_U_EMAIL" --user_pass="$WP_U_PASS" --role="$WP_U_ROLE" --allow-root
 
-#---------------------------------------------------php config---------------------------------------------------#
+#---------------------------------------------------#
+#              PHP-FPM Configuration                #
+#---------------------------------------------------#
 
-# change listen port from unix socket to 9000
+# Update PHP-FPM configuration to listen on port 9000 instead of a Unix socket
 sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
-# create a directory for php-fpm
+
+# Create the required directory for PHP-FPM
 mkdir -p /run/php
-# start php-fpm service in the foreground to keep the container running
+
+# Start PHP-FPM in the foreground
 /usr/sbin/php-fpm7.4 -F
